8846729961e6f1696726b5f6d5059194a6506851
M.O.E.J. The Good <82342827+MOEJ-The-Good@users.noreply.github.com>
2022-10-02 14:27:03 -0400

    Updated code with changes.

--- gamemodes/base/entities/entities/lua_run.lua
+++ gamemodes/base/entities/entities/lua_run.lua
@@ -7,12 +7,12 @@ ENT.DisableDuplicator = true
 AccessorFunc( ENT, "m_bDefaultCode", "DefaultCode" )
 
 local ALLOWED_LUA, ALLOWED_MAPS = include( "cfc_block_luarun/config.lua" )
-local CURRENT_MAP_ALLOWED = table.HasValue( ALLOWED_MAPS, game.GetMap() )
+local CURRENT_MAP_ALLOWED = ALLOWED_MAPS[game.GetMap()]
 
 function ENT:Initialize()
 
 	-- If the entity has its first spawnflag set, run the code automatically
-	if ( self:HasSpawnFlags( SF_LUA_RUN_ON_SPAWN ) ) then
+	if self:HasSpawnFlags( SF_LUA_RUN_ON_SPAWN ) then
 		self:RunCode( self, self, self:GetDefaultCode() )
 	end
 
@@ -20,7 +20,7 @@ end
 
 function ENT:KeyValue( key, value )
 
-	if ( key == "Code" ) then
+	if key == "Code" then
 		self:SetDefaultCode( value )
 	end
 
@@ -31,7 +31,7 @@ function ENT:SetupGlobals( activator, caller )
 	ACTIVATOR = activator
 	CALLER = caller
 
-	if ( IsValid( activator ) and activator:IsPlayer() ) then
+	if IsValid( activator ) and activator:IsPlayer() then
 		TRIGGER_PLAYER = activator
 	end
 
@@ -47,7 +47,9 @@ end
 
 function ENT:RunCode( activator, caller, code )
 
-	if not CURRENT_MAP_ALLOWED or not table.HasValue( ALLOWED_LUA, util.MD5( code ) ) then return end
+	if not CURRENT_MAP_ALLOWED or not ALLOWED_LUA[util.MD5( code )] then
+		return
+	end
 
 	self:SetupGlobals( activator, caller )
 	RunString( code, "lua_run#" .. self:EntIndex() )
@@ -57,8 +59,8 @@ end
 
 function ENT:AcceptInput( name, activator, caller, data )
 
-	if ( name == "RunCode" ) then self:RunCode( activator, caller, self:GetDefaultCode() ) return true end
-	if ( name == "RunPassedCode" ) then self:RunCode( activator, caller, data ) return true end
+	if name == "RunCode" then self:RunCode( activator, caller, self:GetDefaultCode() ) return true end
+	if name == "RunPassedCode" then self:RunCode( activator, caller, data ) return true end
 
 	return false
 
