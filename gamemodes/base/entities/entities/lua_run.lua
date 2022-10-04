d634d3c49b7715c12ecbb7a34e22444363c3f778
M.O.E.J. The Good <82342827+MOEJ-The-Good@users.noreply.github.com>
2022-10-03 20:06:05 -0400

    Tabs to spaces.

--- gamemodes/base/entities/entities/lua_run.lua
+++ gamemodes/base/entities/entities/lua_run.lua
@@ -14,52 +14,52 @@ local LOGGER = Logger( "cfc block luarun" )
 local MD5 = util.MD5
 
 function ENT:Initialize()
-	-- If the entity has its first spawnflag set, run the code automatically
-	if self:HasSpawnFlags( SF_LUA_RUN_ON_SPAWN ) then
-		self:RunCode( self, self, self:GetDefaultCode() )
-	end
+    -- If the entity has its first spawnflag set, run the code automatically
+    if self:HasSpawnFlags( SF_LUA_RUN_ON_SPAWN ) then
+        self:RunCode( self, self, self:GetDefaultCode() )
+    end
 end
 
 function ENT:KeyValue( key, value )
-	if key == "Code" then
-		self:SetDefaultCode( value )
-	end
+    if key == "Code" then
+        self:SetDefaultCode( value )
+    end
 end
 
 function ENT:SetupGlobals( activator, caller )
-	ACTIVATOR = activator
-	CALLER = caller
+    ACTIVATOR = activator
+    CALLER = caller
 
-	if IsValid( activator ) and activator:IsPlayer() then
-		TRIGGER_PLAYER = activator
-	end
+    if IsValid( activator ) and activator:IsPlayer() then
+        TRIGGER_PLAYER = activator
+    end
 end
 
 function ENT:KillGlobals()
-	ACTIVATOR = nil
-	CALLER = nil
-	TRIGGER_PLAYER = nil
+    ACTIVATOR = nil
+    CALLER = nil
+    TRIGGER_PLAYER = nil
 end
 
 function ENT:RunCode( activator, caller, code )
-	local hash = MD5( code )
-	if not CURRENT_MAP_ALLOWED or not ALLOWED_LUA[hash] then
-		if SERVER then
-			LOGGER:warn( '<' .. hash .. '> "' .. code .. '"' )
-		end
+    local hash = MD5( code )
+    if not CURRENT_MAP_ALLOWED or not ALLOWED_LUA[hash] then
+        if SERVER then
+            LOGGER:warn( '<' .. hash .. '> "' .. code .. '"' )
+        end
 
-		return
-	end
+        return
+    end
 
-	self:SetupGlobals( activator, caller )
-	RunString( code, "lua_run#" .. self:EntIndex() )
-	LOGGER:debug( '<' .. hash .. '> "' .. code .. '"' )
-	self:KillGlobals()
+    self:SetupGlobals( activator, caller )
+    RunString( code, "lua_run#" .. self:EntIndex() )
+    LOGGER:debug( '<' .. hash .. '> "' .. code .. '"' )
+    self:KillGlobals()
 end
 
 function ENT:AcceptInput( name, activator, caller, data )
-	if name == "RunCode" then self:RunCode( activator, caller, self:GetDefaultCode() ) return true end
-	if name == "RunPassedCode" then self:RunCode( activator, caller, data ) return true end
+    if name == "RunCode" then self:RunCode( activator, caller, self:GetDefaultCode() ) return true end
+    if name == "RunPassedCode" then self:RunCode( activator, caller, data ) return true end
 
-	return false
+    return false
 end
